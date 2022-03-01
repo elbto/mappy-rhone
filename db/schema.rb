a9f9656460974a5c07a5b442d2351abd91ff5c8a
fassi fihri mohammed <mohammedfassifihri09@gmail.com>
2022-03-01 16:50:21 +0100

    on a le fichier avec les seeds mais on peut pas encore les lancer puisqu il y a pas de zone encore crée

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_134214) do
+ActiveRecord::Schema.define(version: 2022_03_01_135519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,19 @@ ActiveRecord::Schema.define(version: 2022_03_01_134214) do
     t.index ["commune_id"], name: "index_gares_on_commune_id"
   end
 
+  create_table "pharmacies", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.integer "zip_code"
+    t.decimal "lattitude"
+    t.decimal "longitude"
+    t.bigint "commune_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["commune_id"], name: "index_pharmacies_on_commune_id"
+  end
+
   add_foreign_key "ecoles", "communes"
   add_foreign_key "gares", "communes"
+  add_foreign_key "pharmacies", "communes"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+filepath = "db/csvDB/pharmacies.csv"
+
+CSV.foreach(filepath, headers: :first_row) do |row|
+  #dans chaque row on a le nom, l'adresse, le cp, lattitude et longitude de la pharamcie
+  #il faut attendre de créer les zones pour ensuite lié les pharmacie à la zone
+end
