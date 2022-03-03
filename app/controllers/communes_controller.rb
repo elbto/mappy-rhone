48c500df0c217020598d234990de56e8cb3b641a
fassi fihri mohammed <mohammedfassifihri09@gmail.com>
2022-03-03 16:02:30 +0100

    la on arrive a voir les communes en fonction des prix

--- app/controllers/communes_controller.rb
+++ app/controllers/communes_controller.rb
@@ -1,15 +1,34 @@
 class CommunesController < ApplicationController
   def index
-    if params[:query].present?
-      @commune = Commune.find_by(name: params[:query])
+    if params[:name_query].present?
+      @commune = Commune.find_by(name: params[:name_query])
       @gares = @commune.gares
     else
       @communes = Commune.all
     end
     if params[:price_query].present?
       @communes = @communes.where("price < ?", params[:price_query])
-      #("id > ?", 200)
-      #@flats = @flats.where("price_per_night <= ?", params[:max_price])
     end
+  end
+
+  def geojson
+    coordonnes = []
+    @communes = Commune.all
+    @communes = @communes.where('price <= ?', params[:price_query]) if params[:price_query].present?
+    p "la y a le prix #{params[:price_query]}"
+    @communes.each do |commune|
+      coordonnes << commune.polygon[0]
+    end
+
+
+    json = {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Polygon',
+        'coordinates': coordonnes
+      }
+    }
+
+    render json: json
   end
 end
---
+++ app/javascript/controllers/box_controller.js
@@ -0,0 +1,65 @@
+import { Controller } from "@hotwired/stimulus"
+import mapboxgl from "mapbox-gl"
+
+export default class extends Controller {
+  static targets = ['mapContainer']
+  static values = {
+    priceQuery: Number
+  }
+
+  addData() {
+    this.map.on('load', () => {
+      // Add a data source containing GeoJSON data.
+      this.map.addSource('maine', {
+        'type': 'geojson',
+        'data': this.geojson
+      });
+
+      this.map.addLayer({
+          'id': 'maine',
+          'type': 'fill',
+          'source': 'maine',
+          'layout': {},
+          'paint': {
+              'fill-color': '#0080ff',
+              'fill-opacity': 0.5
+          }
+      });
+      // Add a black outline around the polygon.
+      this.map.addLayer({
+          'id': 'outline',
+          'type': 'line',
+          'source': 'maine',
+          'layout': {},
+          'paint': {
+              'line-color': '#000',
+              'line-width': 3
+          }
+      });
+    });
+  }
+
+  fetchGeoJson() {
+    fetch(`/results/geojson?price_query=${this.priceQueryValue}`)
+      .then(response => response.json())
+      .then(data => {
+        this.geojson = data
+        this.addData()
+      })
+  }
+
+  connect() {
+    console.log(this.mapContainerTarget)
+    mapboxgl.accessToken = "pk.eyJ1Ijoiam9sYXp6IiwiYSI6ImNrejhpZmQ5aDFqajUyd3J4OG15bnh6Y3AifQ.BF_g1YetHVCKTbtg3PUpyA"
+
+    this.map = new mapboxgl.Map({
+      container: this.mapContainerTarget,
+      style: 'mapbox://styles/mapbox/light-v10',
+      zoom: 9,
+      center: [4.835659, 45.764043]
+    })
+
+    this.fetchGeoJson()
+
+  }
+}
